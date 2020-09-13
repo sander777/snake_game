da6d1f333ddb0f9f6de672d7b2c0f4115dc201da
CyberSander <cyber.sander777@gmail.com>
2020-09-13 19:35:11 +0300

    Reload option

--- src/main.rs
+++ src/main.rs
@@ -20,31 +20,50 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut app = SnakeApp::new(opengl)
-        .change_size(SIZE)
-        .background_color([0.1, 0.2, 0.1, 1.0])
-        .update_delta(0.06)
-        .snake_color([0.0, 1.0, 0.5, 1.0], [0.0, 0.0, 1.0, 1.0])
-        .init_field((WINDOW_SIZE[0] / SIZE, WINDOW_SIZE[1] / SIZE))
-        .food_count(10)
-        .food_color([1.0, 0.0, 0.0, 1.0]);
+    'app: loop {
+        let mut app = SnakeApp::new(opengl)
+            .change_size(SIZE)
+            .background_color([0.1, 0.2, 0.1, 1.0])
+            .update_delta(0.06)
+            .snake_color([0.0, 1.0, 0.5, 1.0], [0.0, 0.0, 1.0, 1.0])
+            .init_field((WINDOW_SIZE[0] / SIZE, WINDOW_SIZE[1] / SIZE))
+            .food_count(10)
+            .food_color([1.0, 0.0, 0.0, 1.0]);
 
-    let mut event = Events::new(EventSettings::new());
+        let mut event = Events::new(EventSettings::new());
 
-    let mut button: Option<Button> = None;
-    while let Some(e) = event.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            app.render(args);
-        }
-        if let Some(args) = e.update_args() {
-            let mut score = 0;
-            if app.update(args, &mut button, &mut score) == -2 {
-                break;
+        let mut button: Option<Button> = None;
+        while let Some(e) = event.next(&mut window) {
+            if let Some(args) = e.render_args() {
+                app.render(args);
             }
-            window.ctx.window().set_title(score.to_string().as_str());
+            if let Some(args) = e.update_args() {
+                let mut score = 0;
+                if app.update(args, &mut button, &mut score) == -2 {
+                    break;
+                }
+                window.ctx.window().set_title(score.to_string().as_str());
+            }
+            if let Some(args) = e.press_args() {
+                button = Some(args);
+                match button {
+                    Some(press) => {
+                        match press {
+                            Button::Keyboard(key) => {
+                                match key {
+                                    Key::R => {
+                                        continue 'app;
+                                    }
+                                    _ => {}
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    None => {}
+                }
+            }
         }
-        if let Some(args) = e.press_args() {
-            button = Some(args);
-        }
+        break;
     }
 }
